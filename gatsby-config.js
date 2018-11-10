49c7a786dfd29bdef8c5cd2916cf132a7ef7d3c3
Christopher <christopher.00.ramos@gmail.com>
2018-11-09 18:57:17 -0800

    PWA info changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,36 +7,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        sourceMap: true,
-        outFile: './src/styles/main.css',
-        includePaths: ['src/styles'],
-      },
-    },
-    'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `themes`,
         path: `${__dirname}/src/themes/`,
         ignore: ['_defaults'],
@@ -50,6 +20,36 @@ module.exports = {
         ignore: ['_defaults'],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'BetterDocs Discord #1 Plugins & Themes',
+        short_name: 'BetterDocs',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#262626',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sourceMap: true,
+        outFile: './src/styles/main.css',
+        includePaths: ['src/styles'],
+      },
+    },
+    'gatsby-transformer-remark',
     'gatsby-plugin-offline',
   ],
 }
