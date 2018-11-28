0de887536163dd403293ae5530e9d05d7b70f37c
Christopher <christopher.00.ramos@gmail.com>
2018-11-27 18:52:38 -0800

    plugin/tags page completed

--- src/components/pluginTags.js
+++ src/components/pluginTags.js
@@ -14,37 +14,43 @@ const pluginTags = ({ siteTitle }) => (
         >All</AniLink>
         <AniLink 
         className={style.tag}
+        activeClassName={style.active}
         to="/plugins/tags/bandaged-bd/"
         cover
         bg="#262626"
         >BandagedBD</AniLink>
         <AniLink 
         className={style.tag}
+        activeClassName={style.active}
         to="/plugins/tags/enhanced-discord/"
         cover
         bg="#262626"
         >EnhancedDiscord</AniLink>
         <AniLink 
         className={style.tag}
-        to="/"
+        activeClassName={style.active}
+        to="/plugins/#1"
         cover
         bg="#262626"
         >BeautifulDiscord</AniLink>
         <AniLink 
         className={style.tag}
-        to="/"
+        activeClassName={style.active}
+        to="/plugins/#1"
         cover
         bg="#262626"
         >DiscordInjections</AniLink>
         <AniLink 
         className={style.tag}
-        to="/"
+        activeClassName={style.active}
+        to="/plugins/#1"
         cover
         bg="#262626"
         >DiscordInjections</AniLink>
         <AniLink 
         className={style.tag}
-        to="/"
+        activeClassName={style.active}
+        to="/plugins/#1"
         cover
         bg="#262626"
         >DiscordInjections</AniLink>
--- src/templates/plugin-tags.js
+++ src/templates/plugin-tags.js
@@ -1,6 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Layout from '../components/layout'
+import Info from '../components/plugin-info'
+import plugin from '../styles/plugin.module.scss'
+import hero from '../styles/hero.module.scss'
+import Tags from '../components/pluginTags'
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 // Components
 import { Link, graphql } from "gatsby"
@@ -8,31 +13,93 @@ import { Link, graphql } from "gatsby"
 const Software = ({ pageContext, data }) => {
   const { softwares } = pageContext
   const { edges, totalCount } = data.allMarkdownRemark
-  const tagHeader = `${totalCount} post${
+  const tagHeader = `${totalCount} plugin${
     totalCount === 1 ? "" : "s"
-  } tagged with "${softwares}"`
+  } compatible with "${softwares}"`
 
   return (
     <Layout>
-      <div>
-        <h1>{tagHeader}</h1>
-        <ul>
-          {edges.map(({ node }) => {
-            const { title } = node.frontmatter
-            const { slug } = node.fields
-            return (
-              <li key={slug}>
-                <Link to={ '/plugins' + slug}>{title}</Link>
-              </li>
-            )
-          })}
-        </ul>
-        {/*
-                This links to a page that does not yet exist.
-                We'll come back to it!
-              */}
-        <Link to="/plugins/tags">All Softwares</Link>
+    <div className={plugin.pluginsContainer}
+    >
+
+    <section className={plugin.contentWrapper}
+    >
+
+      <div className={hero.heroPlugins}
+      >
+        <div className={hero.container}
+        >
+          <h2 className={hero.h2}
+          >
+          #1 Source for Discord Plugins!
+          </h2> 
+          <p className={hero.p}
+          >
+          Custom JS plugins made by our commuity!
+          </p> 
+        </div>
       </div>
+
+      <div className={plugin.content}
+        >
+        <div className={plugin.wrapper}
+        >
+        <Info />
+        </div>
+      </div>
+
+    </section>
+
+      <section className={plugin.sidebarSearch}>
+        <div className={plugin.searchContainer}>
+          <input 
+          className={plugin.input}
+          placeholder='Search Plugins library'
+          >
+          </input>
+          <div className={plugin.searchOutput}>
+            {tagHeader}
+          </div>
+          <Tags />
+        </div>
+
+            <div className={plugin.Results}>
+            {edges.map(({ node }) => {
+          return (
+              <AniLink 
+              className={plugin.resultCard}
+              activeClassName={plugin.active}
+              to={'plugins' + node.fields.slug}
+              key={node.id}
+              cover
+              bg="#262626"
+              >
+                <div className={plugin.header}
+                >
+                  <span className={plugin.title}
+                  >
+                  {node.frontmatter.title}
+                  </span>
+                  <span className={plugin.author}
+                  >
+                  {node.frontmatter.author}
+                  </span>
+                </div>
+                <div className={plugin.description}
+                >
+                  <p className={plugin.p}
+                  >
+                    {node.excerpt}
+                  </p>
+                </div>
+              </AniLink>
+              )
+            })}
+            </div>
+
+      </section>
+
+    </div>
     </Layout>
   )
 }
@@ -64,18 +131,25 @@ export default Software
 
 export const pageQuery = graphql`
   query($softwares: String) {
-    allMarkdownRemark(
-      filter: { frontmatter: { software: { in: [$softwares] } } collection: { eq: "plugins" } }
-    ) {
+    allMarkdownRemark( filter: { frontmatter: { software: { in: [$softwares] } } collection: { eq: "plugins" } } ) {
       totalCount
       edges {
         node {
-          frontmatter {
-            title
-            software
-          }
+          excerpt
+          html
+          id
           fields {
             slug
+          }
+          frontmatter {
+            path
+            title
+            author
+            github
+            download
+            support
+            layout
+            tags
           }
         }
       }
