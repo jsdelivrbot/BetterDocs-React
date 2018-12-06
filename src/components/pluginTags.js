0031fdc03fa0d137655d2455932e6d19ff99bce0
Christopher <christopher.00.ramos@gmail.com>
2018-12-05 19:41:38 -0800

    Made "softwares" dynamic

--- src/components/pluginTags.js
+++ src/components/pluginTags.js
@@ -1,36 +1,46 @@
 import React from 'react'
 import style from '../styles/tags.module.scss'
-import { Link } from 'gatsby'
+import { Link, StaticQuery } from 'gatsby'
+import kebabCase from "lodash/kebabCase"
 
 const pluginTags = ({ siteTitle }) => (
-    <div className={style.tagsContainer}
-    >
-        <Link 
-        className={style.tag}
-        activeClassName={style.active}
-        to="/plugins/"
-        >All</Link>
-        <Link 
-        className={style.tag}
-        activeClassName={style.active}
-        to="/plugins/softwares/bandaged-bd/"
-        >BandagedBD</Link>
-        <Link 
-        className={style.tag}
-        activeClassName={style.active}
-        to="/plugins/softwares/enhanced-discord/"
-        >EnhancedDiscord</Link>
-        <Link 
-        className={style.tag}
-        activeClassName={style.active}
-        to="/plugins/softwares/beautiful-discord/"
-        >BeautifulDiscord</Link>
-        <Link 
-        className={style.tag}
-        activeClassName={style.active}
-        to="/plugins/softwares/discord-injections/"
-        >DiscordInjections</Link>
-    </div>
+    <StaticQuery
+    query={ graphql`
+    query pluginSoftwareInfo {
+        allMarkdownRemark(filter: {collection: {eq: "plugins"}}) {
+        group(field: frontmatter___software) {
+            fieldValue
+            totalCount
+        }
+          edges {
+            node {
+              frontmatter {
+                title
+                software
+              }
+            }
+          }
+        }
+      }      
+  `}
+    render={data => (
+        <div className={style.tagsContainer}>
+            <Link 
+            className={style.tag}
+            activeClassName={style.active}
+            to="/plugins/"
+            >All</Link>
+            {data.allMarkdownRemark.group.map(software => (
+                <Link 
+                className={style.tag}
+                activeClassName={style.active}
+                to={`/plugins/softwares/${kebabCase(software.fieldValue)}/`}
+                key={software.fieldValue}
+                >{software.fieldValue}</Link>
+            ))}
+        </div>
+        )}
+    />
 )
 
 export default pluginTags
