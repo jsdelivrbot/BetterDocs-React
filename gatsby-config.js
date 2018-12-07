cd9f46b19857404aea71514b418869d6234a4021
Christopher <christopher.00.ramos@gmail.com>
2018-12-06 18:41:00 -0800

    Added google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,21 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-65593962-3",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/demo/**"],
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `media`,
